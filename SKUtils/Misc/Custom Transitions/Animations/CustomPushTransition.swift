0f89adb7b2622f66357edca65b9b3feab105046c
Sergey <sjsoad666@yandex.ru>
2018-06-08 14:36:19 +0300

    no message

--- SKUtils/Misc/Custom Transitions/Animations/CustomPushTransition.swift
+++ SKUtils/Misc/Custom Transitions/Animations/CustomPushTransition.swift
@@ -27,22 +27,28 @@ class CustomPushTransition: NSObject, CustomAnimatedTransitioning {
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         let container = transitionContext.containerView
-        guard let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from) else { return }
+        guard let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to) else { return }
+        guard let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from) else { return }
         guard let toView = transitionContext.view(forKey: UITransitionContextViewKey.to) else { return }
+        guard let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from) else { return }
         reverseTransition == false ? container.addSubview(toView) : container.insertSubview(toView, belowSubview: fromView)
-        let detailView = reverseTransition == false ? toView : fromView
-        toView.frame = reverseTransition == false ?  CGRect(x: fromView.frame.width, y: 0, width: toView.frame.width, height: toView.frame.height) :
-            toView.frame
-        toView.alpha = reverseTransition == false ? 0 : 1
-        toView.layoutIfNeeded()
         
+        var leftTranslation = CGAffineTransform(translationX: -container.frame.width, y: 0)
+        var rightTranslation = CGAffineTransform(translationX: container.frame.width, y: 0)
+        leftTranslation = leftTranslation.scaledBy(x: 0.8, y: 0.8)
+        rightTranslation = rightTranslation.scaledBy(x: 0.8, y: 0.8)
+        
+        toView.transform = reverseTransition == false ? rightTranslation : leftTranslation
+        fromVC.navigationController?.setNavigationBarHidden(true, animated: true)
         UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: { [reverseTransition] in
-            detailView.frame = reverseTransition == false ? fromView.frame :
-                CGRect(x: toView.frame.width, y: 0, width: toView.frame.width, height: toView.frame.height)
-            detailView.alpha = reverseTransition == false ? 1 : 0
+            toView.transform = .identity
+            fromView.transform = reverseTransition == false ? leftTranslation : rightTranslation
+            toVC.navigationController?.setNavigationBarHidden(false, animated: false)
             }, completion: { [weak self] (_) in
                 self?.animationFinished()
                 transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+                toView.transform = .identity
+                fromView.transform = .identity
         })
     }
     
